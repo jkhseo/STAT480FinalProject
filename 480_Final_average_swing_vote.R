8cc1569cbed6870003575c960ec916f0e4cd08b7
IkeBarrash <47797195+IkeBarrash@users.noreply.github.com>
2020-04-23 18:04:02 -0500

    Add files via upload
    Changed to remove NAs instead of finding data for whole states, now give more factors for the ideal state

--- 480_Final_average_swing_vote.R
+++ 480_Final_average_swing_vote.R
@@ -4,8 +4,12 @@ rm(list=ls())
 library(readr)
 library(ggplot2)
 library(tidyverse)
+#Reading in State voting and Deomgraphics Data
 Demographics = read.csv("https://raw.githubusercontent.com/MEDSL/2018-elections-unoffical/master/election-context-2018.csv")
-
+#Removing variables for state elections
+Demographics = Demographics %>% select(-demgov14,-demgov16,-demsen16,-demhouse16,-repgov14,-repgov16,-repsen16,-rephouse16,-othersen16,-otherhouse16,-othergov16,-othergov14)
+#Removing three counties with no data
+Demographics = na.omit(Demographics)
 
 #Because the data comes at the county level, but we're interested in states we need to aggregate the variables to the state level.
 #Here we sum all voter numbers and average demographic data weighted by population
@@ -49,61 +53,11 @@ State_Data = State_Data %>% mutate(Closeness16 = (.001/(.5-Percent_Republican16)
 State_Data %>% ggplot(aes(x=Importance)) + geom_histogram()
 State_Data %>% ggplot(aes(x=Closeness16,y=Closeness12)) + geom_point()
 
-#finding average swing-state weighted on imporance
-#Adding missing race data on Missouri, Virginia, and South Dakota from 2010 Census data
-#Unemployment pulled from 2010 numbers, age was under 18, so extrapolated to under 29, last 3 still missing
-#https://www.census.gov/quickfacts/SD
-State_Data$white_pct[41] = 84.4
-State_Data$black_pct[41] = 2.4
-State_Data$hispanic_pct[41] = 4.1
-State_Data$nonwhite_pct[41] = 100-84.4
-State_Data$foreignborn_pct[41] = 3.5
-State_Data$female_pct[41] = 49.5
-State_Data$age29andunder_pct[41] = 24.7/17*28
-State_Data$age65andolder_pct[41] = 16.6
-State_Data$average_median_hh_inc[41] = 56499
-State_Data$clf_unemploy_pct[41] = 5
-State_Data$lesshs_pct[41] = 100-91.7
-State_Data$lesshs_whites_pct[41] = NA
-State_Data$lesscollege_whites_pct[41] = NA
-State_Data$rural_pct[41] = NA
-
-#https://www.census.gov/quickfacts/fact/table/VA/PST045219
-State_Data$white_pct[46] = 69.5
-State_Data$black_pct[46] = 19.9
-State_Data$hispanic_pct[46] = 96
-State_Data$nonwhite_pct[46] = 100-69.5
-State_Data$foreignborn_pct[46] = 12.3
-State_Data$female_pct[46] = 50.8
-State_Data$age29andunder_pct[46] = 22/17*28
-State_Data$age65andolder_pct[46] = 15.4
-State_Data$average_median_hh_inc[46] = 71564
-State_Data$clf_unemploy_pct[46] = 7.5
-State_Data$lesshs_pct[46] = 100-89.3
-State_Data$lesshs_whites_pct[46] = NA
-State_Data$lesscollege_whites_pct[46] = NA
-State_Data$rural_pct[46] = NA
-
-#https://www.census.gov/quickfacts/fact/table/MO/PST045219
-State_Data$white_pct[25] = 83
-State_Data$black_pct[25] = 11.8
-State_Data$hispanic_pct[25] = 4.3
-State_Data$nonwhite_pct[25] = 100-83
-State_Data$foreignborn_pct[25] = 4.1
-State_Data$female_pct[25] = 50.9
-State_Data$age29andunder_pct[25] = 22.5/17*28
-State_Data$age65andolder_pct[25] = 16.9
-State_Data$average_median_hh_inc[25] = 53560
-State_Data$clf_unemploy_pct[25] = 9.6
-State_Data$lesshs_pct[25] = 100-89.6
-State_Data$lesshs_pct[25] = NA
-State_Data$lesscollege_whites_pct[25] = NA
-State_Data$rural_pct[25] = NA
-
 #finding the median statistics weighted by importance
 Ideal_state = State_Data %>% 
   summarize(white_pct = mean(white_pct*Importance)/mean(Importance),
             black_pct = mean(black_pct*Importance)/mean(Importance),
+            hispanic_pct = mean(hispanic_pct*Importance)/mean(Importance),
             nonwhite_pct = mean(nonwhite_pct*Importance)/mean(Importance),
             foreignborn_pct = mean(foreignborn_pct*Importance)/mean(Importance),
             female_pct = mean(female_pct*Importance)/mean(Importance),
@@ -111,5 +65,8 @@ Ideal_state = State_Data %>%
             age65andolder_pct = mean(age65andolder_pct*Importance)/mean(Importance),
             average_median_hh_inc = mean(average_median_hh_inc*Importance)/mean(Importance),
             clf_unemploy_pct = mean(clf_unemploy_pct*Importance)/mean(Importance),
-            lesshs_pct = mean(lesshs_pct*Importance)/mean(Importance))
-
+            lesshs_pct = mean(lesshs_pct*Importance)/mean(Importance),
+            lesscollege_pct = mean(lesscollege_pct*Importance)/mean(Importance),
+            lesshs_whites_pct = mean(lesshs_whites_pct*Importance)/mean(Importance),
+            lesscollege_whites_pct = mean(lesscollege_whites_pct*Importance)/mean(Importance),
+            rural_pct = mean(rural_pct*Importance)/mean(Importance))
